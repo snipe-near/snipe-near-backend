973c834f49cf18ddd992a9b2648053b66f8c100c
joundy <hafizjoundys@gmail.com>
2022-11-02 23:40:23 +0700

    init api

--- src/Service.js
+++ src/Service.js
@@ -2,6 +2,34 @@ class Service {
 	constructor(repo) {
 		this.repo = repo
 	}
+
+	async snipe(body) {
+		console.log({ body })
+		return {
+			foo: 1,
+		}
+	}
+
+	async getSnipes(skip = 0, limit = 30) {
+		console.log({ skip, limit })
+		return {
+			foo: 1,
+		}
+	}
+
+	async updateSnipe(id, body) {
+		console.log({ id, body })
+		return {
+			foo: 1,
+		}
+	}
+
+	async deleteSnipe(id) {
+		console.log({ id })
+		return {
+			foo: 1,
+		}
+	}
 }
 
 module.exports = Service
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express')
+const bodyParser = require('body-parser')
 const Cache = require('./helpers/Cache')
 const Database = require('./helpers/Database')
 const Repository = require('./Repository')
@@ -15,9 +16,83 @@ const main = async () => {
 	const service = new Service(repository)
 
 	const server = express()
+	server.use(bodyParser.urlencoded({ extended: true }))
+	server.use(bodyParser.json())
 
 	server.get('/', (req, res) => {
-		res.send('Hello world')
+		res.send('ok')
+	})
+
+	server.post('/snipes', async (req, res) => {
+		try {
+			const snipeData = req.body
+			await service.snipe(snipeData)
+
+			res.json({
+				status: 1,
+			})
+		} catch (error) {
+			const message = error.message || err
+			res.status(500).json({
+				status: 0,
+				message: message,
+			})
+		}
+	})
+
+	server.get('/snipes', async (req, res) => {
+		try {
+			let { skip, limit } = req.query
+			skip = parseInt(skip) || 0
+			limit = Math.min(parseInt(limit), 30) || 30
+
+			const results = await service.getSnipes(skip, limit)
+			res.json({
+				status: 1,
+				data: results,
+			})
+		} catch (error) {
+			const message = error.message || err
+			res.status(500).json({
+				status: 0,
+				message: message,
+			})
+		}
+	})
+
+	server.put('/snipes/:snipeId', async (req, res) => {
+		try {
+			const snipeId = req.params.snipeId
+			const snipeData = req.body
+
+			await service.updateSnipe(snipeId, snipeData)
+			res.json({
+				status: 1,
+			})
+		} catch (error) {
+			const message = error.message || err
+			res.status(500).json({
+				status: 0,
+				message: message,
+			})
+		}
+	})
+
+	server.delete('/snipes/:snipeId', async (req, res) => {
+		try {
+			const snipeId = req.params.snipeId
+
+			await service.deleteSnipe(snipeId)
+			res.json({
+				status: 1,
+			})
+		} catch (error) {
+			const message = error.message || err
+			res.status(500).json({
+				status: 0,
+				message: message,
+			})
+		}
 	})
 
 	const port = process.env.PORT || 5000
