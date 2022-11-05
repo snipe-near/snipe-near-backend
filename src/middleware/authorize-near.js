c4d41fa60d6b83b31bd5a0d6887f4691b9b5e61d
joundy <hafizjoundys@gmail.com>
2022-11-05 20:01:52 +0700

    add: log middleware

--- src/middleware/authorize-near.js
+++ src/middleware/authorize-near.js
@@ -9,6 +9,7 @@ module.exports = (near) => {
 				throw new Error('unauthorized')
 			}
 		} catch (error) {
+			console.log(error)
 			return res.status(401).json({
 				status: 0,
 				message: 'unauthorized',
