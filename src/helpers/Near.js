7406cf21433e30a854c96af73c8160891c03889c
joundy <hafizjoundys@gmail.com>
2022-11-05 20:05:43 +0700

    add: log middleware

--- src/helpers/Near.js
+++ src/helpers/Near.js
@@ -12,34 +12,39 @@ const _hexToArr = (str) => {
 
 class Near {
 	async authSignature(authHeader) {
-		const decodeAuthHeader = Base64.decode(authHeader)
-		const [accountId, pubKey, signature] = decodeAuthHeader.split('&')
-		const pubKeyArr = _hexToArr(pubKey)
-		const signatureArr = _hexToArr(signature)
-		const hash = new Uint8Array(sha256.sha256.array(accountId))
-		const verify = nacl.sign.detached.verify(hash, signatureArr, pubKeyArr)
+		try {
+			const decodeAuthHeader = Base64.decode(authHeader)
+			const [accountId, pubKey, signature] = decodeAuthHeader.split('&')
+			const pubKeyArr = _hexToArr(pubKey)
+			const signatureArr = _hexToArr(signature)
+			const hash = new Uint8Array(sha256.sha256.array(accountId))
+			const verify = nacl.sign.detached.verify(hash, signatureArr, pubKeyArr)
 
-		if (!verify) {
-			throw new Error('unauthorized')
+			if (!verify) {
+				throw new Error('unauthorized')
+			}
+
+			const b58pubKey = bs58.encode(Buffer.from(pubKey.toUpperCase(), 'hex'))
+			const response = await axios.post(configs.nearConfig.nodeUrl, {
+				jsonrpc: '2.0',
+				id: 'dontcare',
+				method: 'query',
+				params: {
+					request_type: 'view_access_key',
+					finality: 'final',
+					account_id: accountId,
+					public_key: `ed25519:${b58pubKey}`,
+				},
+			})
+
+			if (response.data.result && response.data.result.error && pubKey !== accountId) {
+				throw new Error('unauthorized')
+			}
+			return accountId
+		} catch (error) {
+			console.log(error)
+			throw error
 		}
-
-		const b58pubKey = bs58.encode(Buffer.from(pubKey.toUpperCase(), 'hex'))
-		const response = await axios.post(configs.nearConfig.nodeUrl, {
-			jsonrpc: '2.0',
-			id: 'dontcare',
-			method: 'query',
-			params: {
-				request_type: 'view_access_key',
-				finality: 'final',
-				account_id: accountId,
-				public_key: `ed25519:${b58pubKey}`,
-			},
-		})
-
-		if (response.data.result && response.data.result.error && pubKey !== accountId) {
-			throw new Error('unauthorized')
-		}
-		return accountId
 	}
 }
 
