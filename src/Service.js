edb736b273755665ee3b8fbd98c6140d5cf30fb8
joundy <hafizjoundys@gmail.com>
2022-11-12 17:08:35 +0700

    use: pinata ipfs

--- src/Service.js
+++ src/Service.js
@@ -200,7 +200,7 @@ class Service {
 		const dataLowerCase = data.toLowerCase()
 		if (dataLowerCase.includes('http://') || dataLowerCase.includes('https://')) {
 			if (dataLowerCase.includes('ipfs.io/ipfs')) {
-				return data.replace('ipfs.io/ipfs', 'ipfs.fleek.co/ipfs')
+				return data.replace('ipfs.io/ipfs', 'gateway.pinata.cloud/ipfs')
 			}
 			return data
 		}
@@ -217,7 +217,7 @@ class Service {
 			hash = new CID(ipfsSplit[1]).toString()
 		}
 
-		return `https://ipfs.fleek.co/ipfs/${hash}`
+		return `https://gateway.pinata.cloud/ipfs/${hash}`
 	}
 
 	_getObjFromExtra(extra) {
